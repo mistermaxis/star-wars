eb847fad41683ad557ac862001b339741cfcf997
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-23 12:55:39 -0300

    Remove useEffect hook

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -1,13 +1,8 @@
-/* eslint-disable no-console */
-import { useEffect } from 'react';
 import { PlanetState, usePlanetContext } from '../context/PlanetContext';
 import PlanetItem from './PlanetItem';
 
 function PlanetList() {
   const { planets, queryPlanets } = usePlanetContext() as PlanetState;
-  useEffect(() => {
-    queryPlanets();
-  }, []);
 
   return (
     <div>{planets.filter((planet) => planet.selected === true)
