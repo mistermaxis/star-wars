826381371512bc9636e809652c619123934a5e3a
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-23 12:59:07 -0300

    Remove planets and queryPlanets

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -2,7 +2,7 @@ import { PlanetState, usePlanetContext } from '../context/PlanetContext';
 import PlanetItem from './PlanetItem';
 
 function PlanetList() {
-  const { planets, queryPlanets } = usePlanetContext() as PlanetState;
+  const {  } = usePlanetContext() as PlanetState;
 
   return (
     <div>{planets.filter((planet) => planet.selected === true)
